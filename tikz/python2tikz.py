9b061765e9af6b333f940496d15c662b26390f39
jiangc4 <jiangchunheng@gmail.com>
2017-03-16 14:47:38 -0400

    Backup of script on local machine

---
+++ tikz/python2tikz.py
@@ -0,0 +1,54 @@
+#!/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import BaggingClassifier
+from sklearn.datasets import make_gaussian_quantiles
+from sklearn.svm import SVC
+
+# Construct dataset
+X1, y1 = make_gaussian_quantiles(cov=2.,
+                                 n_samples=200, n_features=2,
+                                 n_classes=2, random_state=1)
+X2, y2 = make_gaussian_quantiles(mean=(3, 3), cov=1.5,
+                                 n_samples=300, n_features=2,
+                                 n_classes=2, random_state=1)
+X = np.concatenate((X1, X2))
+y = np.concatenate((y1, - y2 + 1))
+
+# Create and fit an boosted svm
+bdt = BaggingClassifier(SVC())
+
+bdt.fit(X, y)
+
+plot_colors = "br"
+plot_step = 0.02
+class_names = "AB"
+
+plt.figure(figsize=(10, 5))
+
+# Plot the decision boundaries
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                     np.arange(y_min, y_max, plot_step))
+
+Z = bdt.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+plt.axis("tight")
+
+# Plot the training points
+for i, n, c in zip(range(2), class_names, plot_colors):
+    idx = np.where(y == i)
+    plt.scatter(X[idx, 0], X[idx, 1],
+                c=c, cmap=plt.cm.Paired,
+                label="Class %s" % n)
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.legend(loc='upper right')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Decision Boundary')
+
+from matplotlib2tikz import save
+save('contour.tex')
---
+++ zzz.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+name="$1"
+ > ./references.bib
+mkdir ./$name/
+mkdir ./$name/figure/
+mkdir ./$name/chart/
+mkdir ./$name/data/
