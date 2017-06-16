03462d9fc17360775dbd04321f95a3070fda43aa
jiangc4 <jiangchunheng@gmail.com>
2017-06-16 17:04:32 -0400

    add txtshop color settings (commands, marks, comments)

---
+++ texshop_color_settings.sh
@@ -0,0 +1,6 @@
+defaults write TeXShop commentred 0.4
+defaults write TeXShop commentgreen 0.4
+defaults write TeXShop commentblue 0.4
+defaults write TeXShop commandred 0.09
+defaults write TeXShop commandgreen 0.28
+defaults write TeXShop commandblue 0.51
